2717e821fd486d550d696408aa896e846596fba7
Cajon Gonzales <cajongonzales@gmail.com>
2021-03-24 13:53:06 -0400

    Read and output yaml

--- src/yamlEventReader.c
+++ src/yamlEventReader.c
@@ -2,42 +2,26 @@
 #include <stdlib.h> // atoi, malloc
 #include <string.h> //strcpy
 #include <yaml.h>
-#include "uthash.h"
+//#include "uthash.h"
 int yamlEventReader(char *filePath)
 {
-// Hash Table Methods
-  struct my_struct {
-    int id;
-    char name[20];
-    char event_type[20];
-    UT_hash_handle hh;
+  struct my_struct{
+    int component_number; // only if Local factors are to be applied
+    char component_type[20]; //required
+    char category[100]; //Global or Local
+    int level; // how deep in the tree are you
+    double value;
+    char key[50];
+    char system[50]; //Named system to apply factors on - if local then null
+    char tree[100]; // try and capture the levels of the tree
   };
-  struct my_struct *yaml_doc = NULL;
-  void add_event(int event_id, char *event_type)
-  {
-    struct my_struct *s;
-    HASH_FIND_INT(yaml_doc, &event_id, s);
-    if (s==NULL) 
-    {
-      s = (struct my_struct*)malloc(sizeof(struct my_struct));
-      s->id = event_id;
-      HASH_ADD_INT(yaml_doc, id, s);
-    }
-    strcpy(s->event_type, event_type);
-  }
-  void delete_all() {
-    struct my_struct *current_doc, *tmp;
-    HASH_ITER(hh, yaml_doc, current_doc, tmp) {
-      HASH_DEL(yaml_doc, current_doc);  /* delete it (users advances to next) */
-      free(current_doc);             /* free it */
-    }
-  }
 
 // LibYAML 
 
   FILE *fh = fopen(filePath, "r");
   yaml_parser_t parser;
   yaml_event_t  event;   /* New variable */
+  struct my_struct yamlData;
     /* Initialize parser */
   if(!yaml_parser_initialize(&parser))
     fputs("Failed to initialize parser!\n", stderr);
@@ -48,8 +32,7 @@ int yamlEventReader(char *filePath)
   yaml_parser_set_input_file(&parser, fh);
 
   /* START new code */
-  int id=1;
-  char event_type[20];
+  int level=0;
   do {
     if (!yaml_parser_parse(&parser, &event)) {
        printf("Parser error %d\n", parser.error);
@@ -61,27 +44,46 @@ int yamlEventReader(char *filePath)
     case YAML_NO_EVENT: puts("No event!"); break;
     /* Stream start/end */
     case YAML_STREAM_START_EVENT: 
-      puts("STREAM START"); 
-      //what event takes place here? log it in the hash?
+      puts("STREAM START");
+      ++level;
       break;
-    case YAML_STREAM_END_EVENT:   puts("STREAM END");   break;
+    case YAML_STREAM_END_EVENT:   
+      puts("STREAM END");
+      --level; 
+      break;
     /* Block delimeters */
-    case YAML_DOCUMENT_START_EVENT: puts("<b>Start Document</b>"); break;
-    case YAML_DOCUMENT_END_EVENT:   puts("<b>End Document</b>");   break;
-    case YAML_SEQUENCE_START_EVENT: puts("<b>Start Sequence</b>"); break;
-    case YAML_SEQUENCE_END_EVENT:   puts("<b>End Sequence</b>");   break;
-    case YAML_MAPPING_START_EVENT:  puts("<b>Start Mapping</b>");  break;
-    case YAML_MAPPING_END_EVENT:    puts("<b>End Mapping</b>");    break;
+    case YAML_DOCUMENT_START_EVENT: 
+      puts("<b>Start Document</b>"); 
+      ++level; 
+      break;
+    case YAML_DOCUMENT_END_EVENT:   
+      puts("<b>End Document</b>");   
+      --level; 
+      break;
+    case YAML_SEQUENCE_START_EVENT: 
+      puts("<b>Start Sequence</b>"); 
+      break;
+    case YAML_SEQUENCE_END_EVENT:   
+      puts("<b>End Sequence</b>");   
+      --level;
+      break;
+    case YAML_MAPPING_START_EVENT:  
+      puts("<b>Start Mapping</b>");  
+      ++level;
+      break;
+    case YAML_MAPPING_END_EVENT:    
+      puts("<b>End Mapping</b>");    
+      --level;
+      break;
     /* Data */
     case YAML_ALIAS_EVENT:
       printf("Got alias (anchor %s)\n", event.data.alias.anchor); 
       break;
       
     case YAML_SCALAR_EVENT:
-      event_type[0] = YAML_SCALAR_EVENT;
-      printf("Got scalar (value %s)\n", event.data.scalar.value); 
-      add_event(id++, event_type);
-      printf("YAML_SCALAR_EVENT: %d\n", event_type[0]);
+      printf("Got a value at level %d (value %s)\n", level, event.data.scalar.value);
+      yamlData.level=level;
+       
       break;
     }
     if(event.type != YAML_STREAM_END_EVENT)
@@ -92,7 +94,7 @@ int yamlEventReader(char *filePath)
 
   /* Cleanup */
   yaml_parser_delete(&parser);
-  delete_all();
+//  delete_all();
   fclose(fh);
   return 0;
 }
