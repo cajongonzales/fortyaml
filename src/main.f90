0ae159ededa0e044f0a2aea3a295c2fcc9d56c5d
Cajon Gonzales <cajongonzales@gmail.com>
2021-04-07 15:22:42 -0400

    Add derived type component

--- src/main.f90
+++ src/main.f90
@@ -8,7 +8,15 @@ interface
   end subroutine yaml_event_reader 
 end interface
 
+type Component
+   integer :: number
+   real :: friction_design_factor
+   character(len=72) :: system
+end type Component   
 
+type(Component) :: volume_Global
+type(Component) :: volume_System
+type(Component) :: volume_Local
 !! Not currently supporting global tags
 !character(kind=c_char, len=*), parameter :: gtag_file = "../yamlFiles/global-tag.yaml" // c_null_char
 character(kind=c_char, len=*), parameter :: nYaml_file = "../yamlFiles/nYaml.yaml" // c_null_char
