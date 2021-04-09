5d3fc21cc90968b134968f87fb89529802eab25e
Cajon Gonzales <cajongonzales@gmail.com>
2021-04-09 15:31:02 -0400

    Add function signature and components to main

--- src/main.f90
+++ src/main.f90
@@ -1,5 +1,5 @@
 program fortBinding
-use iso_c_binding, only: C_CHAR, C_DOUBLE, C_NULL_CHAR, C_INT, C_PTR
+use iso_c_binding, only: C_CHAR, C_DOUBLE, C_NULL_CHAR, C_INT, C_PTR, C_DOUBLE
 implicit none
 interface 
   function yaml_event_reader(filePath) bind(C, name="yamlEventReader")
@@ -22,23 +22,26 @@ type(c_ptr) :: dict_ptr
 
 real(c_double) :: des_fact
 type Component
-   integer :: number
-   real :: friction_design_factor
-   real :: interfactial_design_factor
-   real :: design_factor1
-   real :: design_factor2
-   real :: design_factor3
-   real :: design_factor4
-   real :: design_factor5
-   character(len=72) :: system
+   integer(C_INT) :: number
+   real(C_DOUBLE) :: friction_design_factor
+   real(C_DOUBLE) :: interfactial_design_factor
+   real(C_DOUBLE) :: design_factor1
+   real(C_DOUBLE) :: design_factor2
+   real(C_DOUBLE) :: design_factor3
+   real(C_DOUBLE) :: design_factor4
+   real(C_DOUBLE) :: design_factor5
+   character(kind=C_CHAR, len=72) :: system  
 end type Component   
 
-type(Component) :: volume_Global
-type(Component) :: volume_System
-type(Component) :: volume_Local
-!! Not currently supporting global tags
-!character(kind=c_char, len=*), parameter :: gtag_file = "../yamlFiles/global-tag.yaml" // c_null_char
-character(kind=c_char, len=*), parameter :: nYaml_file = "../yamlFiles/nYaml.yaml" // c_null_char
+type(Component) :: volume_g ! wants only global design factor
+type(Component) :: volume_sys ! wants only system design factor
+type(Component) :: volume_loc ! wants only local design factor
+type(Component) :: volume_sys_loc ! wants system and local
+type(Component) :: volume_g_loc ! wants global and local
+type(Component) :: volume_g_sys ! wants global and system
+type(Component) :: volume_g_sys_loc ! want global system and local
+!character(kind=c_char, len=*), parameter :: gtag_file = "../yamlFiles/global-tag.yaml" // c_null_char !! Not currently supporting
+!global tags
 
 !call yaml_event_reader
 dict_ptr = yaml_event_reader(nYaml_file) 
@@ -63,5 +66,31 @@ write(*,*) des_fact, " should be 2.2354343"
 des_fact = get_var_for_comp(dict_ptr, 108, "jimbob", "Primary" // c_null_char)
 write(*,*) des_fact, " should be 1.0"
 
+des_fact = get_var_for_comp(dict_ptr, 105, "friction_design_factor", "LOCCA_INJECTION")
+write(*,*) des_fact
+character(kind=c_char, len=*), parameter :: nYaml_file = "../yamlFiles/nYaml.yaml" // c_null_char
 
-end program 
+volume_g%number=100
+volume_sys%number=111
+volume_sys%system="Primary"
+volume_loc%number=122
+volume_loc%design_factor5 = 33.333
+volume_sys_loc%number=200
+volume_sys_loc%system="Secondary"
+volume_g_loc%number=300
+volume_g_sys%number=400
+volume_g_sys%system="LOCCA"
+volume_g_sys_loc%number=500
+volume_g_sys_loc%system="Primary"
+!call yaml_event_reader(gtag_file) 
+
+end program fortBinding  
+
+!function isPartofSystemName(comp%system, arg) result(tf)
+!    character(kind=c_char, len=*) :: arg ! from cpp dictionary
+!    character(len=*) :: comp%system      ! from fortran structure
+!    logical :: tf ! true or false
+!
+!    tf = arg == comp%system
+!
+!end function
