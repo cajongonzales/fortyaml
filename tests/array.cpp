8f55f7d5dd606a5ab0eec0bcb0bd0d5ab73a291c
Alicia Klinvex <alicia.klinvex@unnpp.gov>
2021-04-22 18:44:40 -0400

    Make 1 meaningful test
    This also exists as an example of how to use yaml-cpp.

--- tests/array.cpp
+++ tests/array.cpp
@@ -4,9 +4,16 @@
 
 TEST(YamlArray, Array) {
     std::string fileName = "../../yamlFiles/array.yaml";
-    yamlEventReader(fileName.c_str());
+    InputParser* parser = yamlEventReader(fileName.c_str());
     
-    EXPECT_TRUE(false);
+    const YAML::Node& node = parser->get_node();
+
+    EXPECT_TRUE(node.IsSequence());
+    EXPECT_EQ(node.size(), 2);
+    EXPECT_TRUE(node[0].IsScalar());
+    EXPECT_TRUE(node[1].IsScalar());
+    EXPECT_EQ(node[0].as<std::string>(), "member");
+    EXPECT_EQ(node[1].as<std::string>(), "member2");
 }
 	
 int main(int argc, char* argv[]) {
