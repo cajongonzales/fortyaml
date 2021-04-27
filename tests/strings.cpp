c22d0400ce49b81594f197fe128876190f3e2cc4
Alicia Klinvex <alicia.klinvex@unnpp.gov>
2021-04-27 13:26:35 -0400

    Fix a variety of tests including nYaml
    nYAML is the most important test, since it tests the features
    required by our YAML input files.

--- tests/strings.cpp
+++ tests/strings.cpp
@@ -4,9 +4,18 @@
 
 TEST(YamlStrings, Strings) {
     std::string fileName = "../../yamlFiles/strings.yaml";
-    yamlEventReader(fileName.c_str());
+    InputParser* parser = yamlEventReader(fileName.c_str());
     
-    EXPECT_TRUE(false);
+    const YAML::Node& node = parser->get_node();
+
+    EXPECT_TRUE(node.IsMap());
+    EXPECT_EQ(node.size(), 3);
+    EXPECT_TRUE(node["unqouted"].IsScalar());
+    EXPECT_TRUE(node["literal-block"].IsScalar());
+    EXPECT_TRUE(node["folded"].IsScalar());
+    EXPECT_EQ(node["unqouted"].as<std::string>(), "string");
+    EXPECT_EQ(node["literal-block"].as<std::string>(), "This entire block of text will be the value of the 'literal-block' key,\nwith line breaks being preserved.\n");
+    EXPECT_EQ(node["folded"].as<std::string>(), "This entire block of text will be the value of 'folded', but this time, all newlines will be replaced with a single space.\n");
 }
 	
 int main(int argc, char* argv[]) {
--- tests/yamlVersion.cpp
+++ tests/yamlVersion.cpp
@@ -4,9 +4,15 @@
 
 TEST(YamlVersion, YamlVersion) {
     std::string fileName = "../../yamlFiles/yaml-version.yaml";
-    yamlEventReader(fileName.c_str());
+    InputParser* parser = yamlEventReader(fileName.c_str());
     
-    EXPECT_TRUE(false);
+    const YAML::Node& node = parser->get_node();
+
+    EXPECT_TRUE(node.IsSequence());
+    EXPECT_EQ(node.size(), 3);
+    EXPECT_EQ(node[0].as<int>(), 1);
+    EXPECT_EQ(node[1].as<int>(), 2);
+    EXPECT_EQ(node[2].as<int>(), 3);
 }
 	
 int main(int argc, char* argv[]) {
